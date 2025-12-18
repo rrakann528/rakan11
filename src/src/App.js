0a9188ae47ba465c70e235cd2f2650e461f864ce
rrakann528 <rrakann528@gmail.com>
2025-12-18 18:57:58 +0300

    Create App.js with video player and chat components

---
+++ src/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="video-player">[مشغل الفيديو هنا]</div>
+      <div className="chat-container">[الدردشة هنا]</div>
+    </div>
+  );
+};
+
+export default App;
